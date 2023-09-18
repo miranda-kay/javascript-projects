095956c944828510fb6d07c8a8217eecbade92ce
miranda-kay <mirandakoesterer@gmail.com>
2023-09-18 16:48:40 -0500

    Updated

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -2,7 +2,7 @@ let num = 1001;
 
 //Returns 'undefined'.
 console.log(num.length);
-
+//will get back to this
 //Use type conversion to print the length (number of digits) of an integer.
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
