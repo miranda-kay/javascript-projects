f81408a3be03f9fab198ff83c8998c0f3d789c60
miranda-kay <mirandakoesterer@gmail.com>
2023-09-21 15:55:48 -0500

    Updated

---
+++ loops/Notes
@@ -0,0 +1,5 @@
+let phrase = "Chili Cook-off";
+
+for (let i = 0; i < phrase.length - 1; i = i + 3) {
+   console.log(phrase[i]);
+}
--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -1,20 +1,43 @@
 /*Exercise #1: Construct for loops that accomplish the following tasks:
-    a. Print the numbers 0 - 20, one number per line.
-    b. Print only the ODD values from 3 - 29, one number per line.
-    c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
-    d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+    a. Print the numbers 0 - 20, one number per line. */
 
+for (let i = 0; i < 21; i++) {
+	console.log(i);
+}
 
+  //  b. Print only the ODD values from 3 - 29, one number per line.//
+  for (let i = 3; i < 30; i = i + 2) {
+    console.log(i);
+  }
+
+ //   c. Print the EVEN numbers 12 to -14 in descending order, one number per line. //
+ for (let i = 12; i > -15; i = i - 2) {
+  console.log(i);
+}
+//    d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). //
+for (let i = 50; i > 19; i = i / 3) {
+  console.log(i);
+}
 
 
 /*Exercise #2: 
-Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
+Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42]. */
 
+let course = "LaunchCode";
+let courseArray = [1, 5, 'LC101', 'blue', 42];
 
-Construct ``for`` loops to accomplish the following tasks:
-  a. Print each element of the array to a new line.
-  b. Print each character of the string - in reverse order - to a new line. */ 
+/*Construct ``for`` loops to accomplish the following tasks:
+  a. Print each element of the array to a new line. */
 
+for (let i = 0; i < courseArray.length; i++) {
+  console.log(courseArray[i]);
+}
+
+  /*b. Print each character of the string - in reverse order - to a new line. */ 
+
+  for (let i = 0; i < courseArray.length; i++) {
+    console.log(courseArray[i]);
+  }
 
 
 
@@ -22,3 +45,12 @@ Construct ``for`` loops to accomplish the following tasks:
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+  let numArray = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+  let evens = [2, 18, 38, -10, 0, 104], odds = [3, 13, -5, 11];
+
+  for (let i = 0; i < numArray.length; i++) {
+    console.log(evens);
+    console.log(odds);
+  }
+  
